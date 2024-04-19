977ec20d6007df193988d01ff170d1e2fb337fd1
ranepaarth <2019paarth.rane@ves.ac.in>
2024-04-19 12:50:47 +0530

    feat: register form

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,8 @@
+import { RegisterForm } from '@/components/auth/RegisterForm';
+import React from 'react';
+
+function RegisterPage() {
+  return <RegisterForm />;
+}
+
+export default RegisterPage;
---
+++ src/components/auth/RegisterForm.tsx
@@ -0,0 +1,114 @@
+'use client';
+import { registerAction } from '@/actions/register';
+import { CardWrapper } from '@/components/auth/CardWrapper';
+import { FormError } from '@/components/FormError';
+import { FormSuccess } from '@/components/FormSuccess';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { RegisterSchema } from '@/schemas';
+import { zodResolver } from '@hookform/resolvers/zod';
+import React, { useState, useTransition } from 'react';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+export function RegisterForm() {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+  const [isPending, startTransition] = useTransition();
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      name: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof RegisterSchema>) => {
+    setError('');
+    setSuccess('');
+    startTransition(() =>
+      registerAction(values).then(data => {
+        setError(data.error);
+        setSuccess(data.success);
+      }),
+    );
+  };
+
+  return (
+    <CardWrapper
+      headerLabel='Create an Account'
+      backButtonLabel={`Already have an account?`}
+      backButtonHref='/auth/login'
+      showSocial
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+          <div className='space-y-4'>
+            <FormField
+              control={form.control}
+              name='name'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder='Example Name'
+                      type='text'
+                      autoFocus
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='email'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder='placeholder@example.com'
+                      type='email'
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='password'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder='******' type='password' />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button type='submit' className='w-full'>
+            Create Account
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+}
