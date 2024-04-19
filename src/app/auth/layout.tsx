1310b4d3b8cf80b57b3b9504b1e5879bc086fab7
ranepaarth <2019paarth.rane@ves.ac.in>
2024-04-19 12:47:42 +0530

    feat: auth layout

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className='flex h-full items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
+      {children}
+    </div>
+  );
+}
+
+export default AuthLayout;
