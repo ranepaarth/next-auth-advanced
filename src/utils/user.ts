4fd315922bdfe5d7ce89af1bae5a6f3ce8f72712
ranepaarth <2019paarth.rane@ves.ac.in>
2024-04-20 20:02:58 +0530

    chore: utility file
    a file which contain functions which makes use of a logic multiple times

---
+++ src/utils/user.ts
@@ -0,0 +1,29 @@
+import { db } from '@/lib/db';
+
+export async function getUserByEmail(email: string) {
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    return user;
+  } catch (error) {
+    return null;
+  }
+}
+
+export async function getUserById(id: string) {
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return user;
+  } catch (error) {
+    return null;
+  }
+}
