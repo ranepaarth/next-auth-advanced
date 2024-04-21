f91a71f04a8b552304ddc55613bc927944fe121f
ranepaarth <2019paarth.rane@ves.ac.in>
2024-04-21 20:11:39 +0530

    chore: defining routes
    defining routes to invoke the middleware depending on the user session and the requested route

---
+++ src/routes.ts
@@ -0,0 +1,26 @@
+/**
+ * An array of routes accessible to the public
+ * These routes do not require authentication
+ * @type {string[]}
+ */
+export const publicRoutes = ['/'];
+
+/**
+ * An array of routes used for authentication
+ * These routes redirect logged in user to /settings
+ * @type {string[]}
+ */
+export const authRoutes = ['/auth/login', '/auth/register'];
+
+/**
+ * The Prefix for API authentication routes
+ * Routes starting with this prefix are used for API authentication purpose
+ * @type {string}
+ */
+export const apiAuthPrefix = '/api/auth';
+
+/**
+ * Default redirect path after user logs in
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = '/settings';
