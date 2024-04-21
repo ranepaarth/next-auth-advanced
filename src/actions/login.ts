a23982b7500a4df709fa306dcef1f4a17add48f9
ranepaarth <2019paarth.rane@ves.ac.in>
2024-04-21 20:46:40 +0530

    feat: signIn with credentials
    implement server action for signing in user with credentials and redirect on success

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,6 +1,9 @@
 'use server';
 
+import { signIn } from '@/auth';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 import { LoginSchema } from '@/schemas';
+import { AuthError } from 'next-auth';
 import * as z from 'zod';
 
 export async function loginAction(values: z.infer<typeof LoginSchema>) {
@@ -11,7 +14,28 @@ export async function loginAction(values: z.infer<typeof LoginSchema>) {
       error: 'Invalid fields!',
     };
 
-  return {
-    success: 'Email sent!',
-  };
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+    return {
+      success: 'Fine!',
+    };
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return {
+            error: 'Invalid Credentials!',
+          };
+        default:
+          return { error: 'Something went wrong!' };
+      }
+    }
+    throw error;
+  }
 }
