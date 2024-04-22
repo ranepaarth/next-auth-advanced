43eea35e160ab2f5a37ce6b225d0f9a9877d856b
ranepaarth <2019paarth.rane@ves.ac.in>
2024-04-22 12:10:52 +0530

    feat: jwt and session callbacks
    extended the jwt token to include the role property of the user and then user session uses these sub(id of the user) and role properties

--- src/auth.ts
+++ src/auth.ts
@@ -2,8 +2,29 @@ import authConfig from '@/auth.config';
 import { db } from '@/lib/db';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth from 'next-auth';
+import { getUserById } from './utils/user';
+import { UserRole } from '@prisma/client';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (!token.sub || !session.user) return session;
+      if (token.sub && session.user) {
+        session.user.id = token.sub as string;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+    jwt: async ({ token }) => {
+      if (!token.sub) return token;
+      const user = await getUserById(token.sub as string);
+      if (!user) return token;
+      token.role = user.role;
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: {
     strategy: 'jwt',
