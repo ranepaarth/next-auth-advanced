2e1b7f07497e36a47e5d0a45dbac340aa21c4fdd
ranepaarth <2019paarth.rane@ves.ac.in>
2024-04-22 20:26:28 +0530

    feat: OAuth Login providers
    implement google and github auth providers

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -3,9 +3,19 @@ import { getUserByEmail } from '@/utils/user';
 import bcrypt from 'bcryptjs';
 import type { NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
+import Github from 'next-auth/providers/Github';
+import Google from 'next-auth/providers/Google';
 
 export default {
   providers: [
+    Github({
+      clientId: process.env.AUTH_GITHUB_CLIENT_ID,
+      clientSecret: process.env.AUTH_GITHUB_CLIENT_SECRET,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
@@ -25,4 +35,5 @@ export default {
       },
     }),
   ],
+  // secret: process.env.AUTH_SECRET,
 } satisfies NextAuthConfig;
--- src/components/auth/Social.tsx
+++ src/components/auth/Social.tsx
@@ -1,17 +1,35 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
+import { signIn } from 'next-auth/react';
 import React from 'react';
 import { FaGithub } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
 
 export function Social() {
+  const handleLogin = (provider: 'google' | 'github') => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
+
   return (
     <div className='flex w-full items-center gap-x-2'>
-      <Button size='lg' className='w-full' variant='outline'>
+      <Button
+        size='lg'
+        className='w-full'
+        variant='outline'
+        onClick={() => handleLogin('google')}
+      >
         <FcGoogle className='h-5 w-5' />
       </Button>
-      <Button size='lg' className='w-full' variant='outline'>
+      <Button
+        size='lg'
+        className='w-full'
+        variant='outline'
+        onClick={() => handleLogin('github')}
+      >
         <FaGithub className='h-5 w-5' />
       </Button>
     </div>
