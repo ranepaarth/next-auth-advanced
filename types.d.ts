ae048d0085c089063f70c7fb19a9c0457eb48dd1
ranepaarth <2019paarth.rane@ves.ac.in>
2024-04-22 12:05:33 +0530

    chore: type ExtendedUser
    create a type to extend the user type provided by the DefaultSession to include properties like id and role

---
+++ types.d.ts
@@ -0,0 +1,12 @@
+import { UserRole } from '@prisma/client';
+import NextAuth, { type DefaultSession } from 'next-auth';
+
+type ExtendedUser = DefaultSession['user'] & {
+  id: string;
+  role: UserRole;
+};
+declare module 'next-auth' {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
