e359193797300242e41ed9f18bc2b7b842e7ca30
ranepaarth <2019paarth.rane@ves.ac.in>
2024-04-23 23:56:56 +0530

    feat: new-verification route
    A client component that displays the token verification (i.e. email verification) status to the user once they click the verification link sent to their unverified email id.

---
+++ src/app/auth/new-verification/page.tsx
@@ -0,0 +1,8 @@
+import NewVerificationForm from '@/components/auth/NewVerificationForm';
+import React from 'react';
+
+function NewVerification() {
+  return <NewVerificationForm />;
+}
+
+export default NewVerification;
---
+++ src/components/auth/NewVerificationForm.tsx
@@ -0,0 +1,53 @@
+'use client';
+import { verifyToken } from '@/actions/new-verification';
+import { CardWrapper } from '@/components/auth/CardWrapper';
+import { FormError } from '@/components/FormError';
+import { FormSuccess } from '@/components/FormSuccess';
+import { useSearchParams } from 'next/navigation';
+import React, { useCallback, useEffect, useState } from 'react';
+import { BeatLoader } from 'react-spinners';
+
+function NewVerificationForm() {
+  const [error, setError] = useState<string | undefined>('');
+  const [success, setSuccess] = useState<string | undefined>('');
+
+  const searchParams = useSearchParams();
+  const token = searchParams.get('token');
+
+  const onSubmit = useCallback(() => {
+    console.log(searchParams.get('token'));
+    if (!token) {
+      setError('Missing Token');
+      return;
+    }
+
+    verifyToken(token)
+      .then(data => {
+        console.log(data);
+        setSuccess(data.success);
+        setError(data.error);
+      })
+      .catch(() => {
+        setError('Something went wrong!');
+      });
+  }, []);
+
+  useEffect(() => {
+    onSubmit();
+  }, [onSubmit]);
+  return (
+    <CardWrapper
+      headerLabel='Confirm Verification'
+      backButtonHref='/auth/login'
+      backButtonLabel='Back to Login'
+    >
+      <div className='flex w-full flex-col items-center justify-center gap-y-4'>
+        {!error && <BeatLoader />}
+        <FormSuccess message={success} />
+        <FormError message={error} />
+      </div>
+    </CardWrapper>
+  );
+}
+
+export default NewVerificationForm;
