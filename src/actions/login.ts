b6269fb0ee24084a4336798c4e6685fbc1572d36
ranepaarth <2019paarth.rane@ves.ac.in>
2024-04-28 15:14:23 +0530

    feat: login action includes 2FA verification
    Implement a check to create a 2FA code and send the email of the same to the provided email during login. It first checks if 2FA is enabled, if yes then it checks for the input code field. If we have the code it then retrieves the 2FA token using the email and checks if that token is equal to the code provided or has expired. If not then it deletes that token from the db. It then creates the tow-factor confirmation if not already present else deletes the confirmation.

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,11 +1,15 @@
 'use server';
 
 import { signIn } from '@/auth';
-import { sendVerificationEmail } from '@/lib/mail';
-import { generateVerificationToken } from '@/lib/token';
+import { db } from '@/lib/db';
+import { sendTwoFactorTokenEmail, sendVerificationEmail } from '@/lib/mail';
+import { generateTwoFactorToken, generateVerificationToken } from '@/lib/token';
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 import { LoginSchema } from '@/schemas';
+import { getTwoFactorConfirmationByUserId } from '@/utils/tow-factor-confirmation';
+import { getTwoFactorTokenByEmail } from '@/utils/two-factor-token';
 import { getUserByEmail } from '@/utils/user';
+import bcrypt from 'bcryptjs';
 import { AuthError } from 'next-auth';
 import * as z from 'zod';
 
@@ -17,19 +21,36 @@ export async function loginAction(values: z.infer<typeof LoginSchema>) {
       error: 'Invalid fields!',
     };
 
-  const { email, password } = validatedFields.data;
+  const { email, password, code } = validatedFields.data;
 
   const existingUser = await getUserByEmail(email);
 
-  if (!existingUser || !existingUser.email || !existingUser.password)
+  if (!existingUser) {
+    return {
+      error: 'Email does not exist!',
+    };
+  }
+
+  const existingUserId = existingUser.id;
+  const existingUserEmail = existingUser.email;
+  const existingUserPassword = existingUser.password;
+  const isEmailVerified = existingUser.emailVerified;
+  const isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
+
+  if (!existingUserEmail || !existingUserPassword)
     return {
       error: 'Email does not exist!',
     };
 
-  if (!existingUser.emailVerified) {
-    const verificationToken = await generateVerificationToken(
-      existingUser.email,
-    );
+  if (!(await bcrypt.compare(password, existingUserPassword))) {
+    return {
+      error: 'Invalid Password!',
+    };
+  }
+
+  if (!isEmailVerified) {
+    const verificationToken =
+      await generateVerificationToken(existingUserEmail);
 
     await sendVerificationEmail(
       verificationToken.email,
@@ -39,6 +60,54 @@ export async function loginAction(values: z.infer<typeof LoginSchema>) {
     return {
       success: 'Confirmation email sent',
     };
+  }
+
+  if (isTwoFactorEnabled && existingUserEmail) {
+    if (code) {
+      // Done: Verify OTP code
+      const twoFactorToken = await getTwoFactorTokenByEmail(existingUserEmail);
+      if (!twoFactorToken || twoFactorToken.token !== code)
+        return {
+          error: 'Invalid Authentication code!',
+        };
+
+      const hasExpired = new Date(twoFactorToken.expires) < new Date();
+      if (hasExpired) {
+        return {
+          error: 'Authentication code expired!',
+        };
+      }
+
+      await db.twoFactorToken.delete({
+        where: {
+          id: twoFactorToken.id,
+        },
+      });
+
+      const existingConfirmation =
+        await getTwoFactorConfirmationByUserId(existingUserId);
+      if (existingConfirmation) {
+        await db.twoFactorConfirmation.delete({
+          where: {
+            id: existingConfirmation.id,
+          },
+        });
+      }
+
+      await db.twoFactorConfirmation.create({
+        data: {
+          userId: existingUserId,
+        },
+      });
+    } else {
+      const twoFactorToken = await generateTwoFactorToken(existingUserEmail);
+
+      await sendTwoFactorTokenEmail(twoFactorToken.email, twoFactorToken.token);
+
+      return {
+        twoFactor: true,
+      };
+    }
   }
 
   try {
@@ -57,10 +126,6 @@ export async function loginAction(values: z.infer<typeof LoginSchema>) {
         case 'CredentialsSignin':
           return {
             error: 'Invalid email or password',
-          };
-        case 'AccessDenied':
-          return {
-            error: 'Please verify your email!',
           };
         default:
           return { error: 'Something went wrong!' };
