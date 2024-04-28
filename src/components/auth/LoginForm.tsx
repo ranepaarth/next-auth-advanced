3c4e6b3e6e0a57f78e5e59abf5611f0d11645620
ranepaarth <2019paarth.rane@ves.ac.in>
2024-04-28 15:20:52 +0530

    feat: 2FA code input form field
    Utilised the InputOTP field from shadcn/ui, so when user tries login using credentials and if it has 2FA enabled on submission for login using credentials, it toggles the showTwoFactorInput state and depending on that state value it displays the form field accordingly.

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,12 +1,15 @@
 'use client';
+
 import { loginAction } from '@/actions/login';
 import { CardWrapper } from '@/components/auth/CardWrapper';
 import { FormBtn } from '@/components/FormBtn';
 import { FormError } from '@/components/FormError';
 import { FormSuccess } from '@/components/FormSuccess';
+import { Button } from '@/components/ui/button';
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -20,7 +23,7 @@ import { useSearchParams } from 'next/navigation';
 import React, { useState, useTransition } from 'react';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
-import { Button } from '../ui/button';
+import { InputOTP, InputOTPGroup, InputOTPSlot } from '../ui/input-otp';
 
 export function LoginForm() {
   const searchParams = useSearchParams();
@@ -29,6 +32,7 @@ export function LoginForm() {
       ? 'Email already in use with different provider!'
       : '';
 
+  const [showTwoFactorInput, setShowTwoFactorInput] = useState(false);
   const [error, setError] = useState<string | undefined>();
   const [success, setSuccess] = useState<string | undefined>();
   const [isPending, startTransition] = useTransition();
@@ -37,6 +41,7 @@ export function LoginForm() {
     defaultValues: {
       email: '',
       password: '',
+      code: '',
     },
   });
 
@@ -44,10 +49,22 @@ export function LoginForm() {
     setError('');
     setSuccess('');
     startTransition(() =>
-      loginAction(values).then(data => {
-        setError(data.error);
-        setSuccess(data.success);
-      }),
+      loginAction(values)
+        .then(data => {
+          if (data?.error) {
+            setError(data.error);
+          }
+          if (data?.success) {
+            form.reset();
+            setSuccess(data.success);
+          }
+          if (data?.twoFactor) {
+            setShowTwoFactorInput(true);
+          }
+        })
+        .catch(() => {
+          setError('Something went wrong!');
+        }),
     );
   };
 
@@ -61,49 +78,94 @@ export function LoginForm() {
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
           <div className='space-y-4'>
-            <FormField
-              control={form.control}
-              name='email'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      placeholder='placeholder@example.com'
-                      type='email'
-                      autoFocus
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name='password'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input {...field} placeholder='******' type='password' />
-                  </FormControl>
-                  <Button
-                    variant={'link'}
-                    size={'sm'}
-                    asChild
-                    className='px-0 font-normal'
-                  >
-                    <Link href={'/auth/reset-password'}>Forgot Password?</Link>
-                  </Button>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+            {!showTwoFactorInput && (
+              <>
+                <FormField
+                  control={form.control}
+                  name='email'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder='placeholder@example.com'
+                          type='email'
+                          autoFocus
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name='password'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder='******'
+                          type='password'
+                        />
+                      </FormControl>
+                      <Button
+                        variant={'link'}
+                        size={'sm'}
+                        asChild
+                        className='px-0 font-normal'
+                      >
+                        <Link href={'/auth/reset-password'}>
+                          Forgot Password?
+                        </Link>
+                      </Button>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </>
+            )}
+            {showTwoFactorInput && (
+              <FormField
+                control={form.control}
+                name='code'
+                defaultValue=''
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Authentication Code</FormLabel>
+                    <FormControl>
+                      <InputOTP
+                        maxLength={6}
+                        {...field}
+                        onComplete={form.handleSubmit(onSubmit)}
+                      >
+                        <InputOTPGroup>
+                          <InputOTPSlot index={0} />
+                          <InputOTPSlot index={1} />
+                          <InputOTPSlot index={2} />
+                          <InputOTPSlot index={3} />
+                          <InputOTPSlot index={4} />
+                          <InputOTPSlot index={5} />
+                        </InputOTPGroup>
+                      </InputOTP>
+                    </FormControl>
+                    <FormDescription>
+                      Please enter the authentication code sent to your email.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            )}
           </div>
           <FormError message={error || isUrlError} />
           <FormSuccess message={success} />
-          <FormBtn isPending={isPending} text={'Log In'} />
+          <FormBtn
+            isPending={isPending}
+            text={!showTwoFactorInput ? 'Log In' : 'Confirm'}
+          />
         </form>
       </Form>
     </CardWrapper>
