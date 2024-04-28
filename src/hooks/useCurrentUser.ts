c2c4c6129283ea785dc3bf299a6fe9a95f8861a2
ranepaarth <2019paarth.rane@ves.ac.in>
2024-04-28 20:36:39 +0530

    feat: custom current user hook
    A hook to retrieve the logged in user from the session made available by the useSession hook. For this to work the component that uses the custom user hook must be wrapped inside SessionProver and needs to be a client component.

---
+++ src/hooks/useCurrentUser.ts
@@ -0,0 +1,10 @@
+'use client';
+import { useSession } from 'next-auth/react';
+
+const useCurrentUser = () => {
+  const session = useSession();
+
+  return session.data?.user;
+};
+
+export default useCurrentUser;
