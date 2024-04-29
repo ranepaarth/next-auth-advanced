2d0d26503af70ae4c1f6c028a010f7a2904544da
ranepaarth <2019paarth.rane@ves.ac.in>
2024-04-30 01:08:12 +0530

    build: fix import casing
    The providers import destination was in capital casing instead of small casing

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -3,8 +3,8 @@ import { getUserByEmail } from '@/utils/user';
 import bcrypt from 'bcryptjs';
 import type { NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
-import Github from 'next-auth/providers/Github';
-import Google from 'next-auth/providers/Google';
+import Github from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
 
 export default {
   providers: [
